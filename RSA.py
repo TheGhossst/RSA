07ae9b7029a772c9b85cd2805782ffe24a7fdce2
TheGhossst <nandukrishnapkm97@gmail.com>
2023-11-26 18:12:33 +0530

    add : function isprime

--- RSA.py
+++ RSA.py
@@ -1,3 +1,11 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 def rsa():
     p = int(input("Enter a large prime number (p): "))
     while not is_prime(p):

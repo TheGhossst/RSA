ba510f6e2bc4f729b2825e9b0314f34893bf0e6e
TheGhossst <nandukrishnapkm97@gmail.com>
2023-11-26 18:05:49 +0530

    add main

--- RSA.py
+++ RSA.py
@@ -2,3 +2,7 @@ def main():
     public_key, private_key, encrypt, decrypt = rsa()
 
     message = int(input("Enter a message to encrypt (as an integer): "))
+    
+    encrypted_message = encrypt(message)
+    print("Public key is: ",public_key)
+    print("Encrypted message:", encrypted_message)
